66151a56093a2c99b56c89998c965dd516981f93
Jan Trojak <trojaj12@fit.cvut.cz>
2022-06-05 13:16:29 +0200

    Make props nollable forgiving

--- UnknownProject/Api/Controllers/AuthController.cs
+++ UnknownProject/Api/Controllers/AuthController.cs
@@ -157,7 +157,7 @@ public class AuthController : ControllerBase
 }
 public class UserDTO
 {
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string Username { get; set; }
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+    public string Username { get; set; } = null!;
 }
